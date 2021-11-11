8fbcf15abc29b0ec976e99e8639daeb6c77ad25d
Savan Doshi <savandoshi@Savans-MacBook-Pro.local>
2021-11-11 18:20:47 -0500

    readjusted the csv data:

--- json_to_dataframe.py
+++ json_to_dataframe.py
@@ -1,40 +1,34 @@
 import pandas as pd
 import json
-def convert(script):
-    character_to_lines = dict()
-    characters = set()
-
+def convert(script, startid):
+    character_to_lines = {
+        "id": [],
+        "character": [],
+        "line #": [],
+        "line": []
+    }
+    line_number = 0
     for part in script["movie_script"]:
-        if part["type"] == "speech" and part["character"] != "":
-            line = part["text"]
-            if part["character"] not in characters:
-                characters.add(part["character"])
-                character_to_lines[part["character"]] = [line]
-            else:
-                character_to_lines[part["character"]].append(line)
+        if part["type"] == "speech" and "character" in part and  part["character"] != "":
+            character_to_lines["line"].append(part["text"])
+            character_to_lines["character"].append(part["character"])
+            character_to_lines["line #"].append(line_number)
+            character_to_lines["id"].append(startid)
+            line_number += 1
+            startid += 1
 
     return character_to_lines
 
-def pad_dict_list(dict_list, padel):
-    lmax = 0
-    for lname in dict_list.keys():
-        lmax = max(lmax, len(dict_list[lname]))
-    for lname in dict_list.keys():
-        ll = len(dict_list[lname])
-        if  ll < lmax:
-            dict_list[lname] += [padel] * (lmax - ll)
-    return dict_list            
 
 
-
-f = open('Movies/Coco.json', "r")
+f = open('Movies/TheSocialNetwork.json', "r")
  
 # Reading from file
 data = json.loads(f.read())
-line_dict = convert(data)
-line_dict = pad_dict_list(line_dict, " ")
+line_dict = convert(data, 6140)
+
 line_dict = pd.DataFrame.from_dict(line_dict)
 
 print(line_dict.head())
-line_dict.to_csv('Movie_spreadsheets/coco.csv')
+line_dict.to_csv('Movie_spreadsheets/thesocialnetwork.csv')
 
